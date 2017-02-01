7aaf04d736f24c899b27a1caee578fbeec4fb4ed
Jonathan Ellington <ellingtonjp@gmail.com>
2017-01-31 23:07:53 -0800

    Add demo program

--- src/main.cpp
+++ src/main.cpp
@@ -3,6 +3,6 @@
 #include <string>
 
 int main(int argc, char* argv[]) {
-  std::cout << "pow2(" << argv[0] << " = " pow2(std::stod(argv[0])) << std::endl;
+  std::cout << "pow2(" << argv[1] << ")" << " = " << pow2(std::stod(argv[1])) << std::endl;
   return 0;
 }
